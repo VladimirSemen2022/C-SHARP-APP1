424de2d31a38802697300b66d4a8154e92bee01e
User <User@DESKTOP-MV43C0T>
2022-06-11 12:40:18 +0300

    Добавьте файлы проекта.

---
+++ C SHARP APP1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_SHARP_APP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            for (int c = 0; c < name.Length; c++)
+                Console.WriteLine(name[c]);
+        }
+    }
+}
